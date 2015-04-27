647ab064a09807a3dd1bc36060ec727b7c8eb7c2
Miguel Angel Bonachea Soto <desarrollowebuno@gmail.com>
2015-04-27 17:55:51 -0500

    Change layout of footer to three columns

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,17 @@
 		<div class="navbar navbar-inverse footer">
-			<div class="container">			
-				&copy; 2014 Miguel A. Bonachea Soto
+			<div class="row">		
+				<div class="col-sm-4 text-left">
+					&copy; 2014 folat-edu.net
+				</div>
+				<div class="col-sm-4 text-center">
+					<a href="<?php echo base_url('pages/terms');?>">Terms and Conditions</a>
+				</div>
+				<div class="col-sm-4 text-right">
+					<a href="<?php echo base_url();?>">
+						<span class="footer_logo_text">FOLAT</span>
+		            	<img class="footer_logo" src="<?php echo base_url();?>/images/folat-hedge-hog-small.png">
+		            </a>
+				</div>
 			</div>
 		</div>
 	</div> <!--/main-container-->
